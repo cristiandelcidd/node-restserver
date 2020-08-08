b9af2dc3b649be5abd5267d9c9f6f478ec206bb4
Cristian Delcid <falet5080@hotmail.com>
2020-08-07 18:15:26 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://falet:tG7WLvZFTiTwkaiW@cluster0.xdadt.mongodb.net/cafe'
+    // urlDB = 'mongodb+srv://falet:tG7WLvZFTiTwkaiW@cluster0.xdadt.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
